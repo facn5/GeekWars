79321ca0f017f9994d85bd53da0350bee37dd2d4
Tamer Nasser <tamernasser@outlook.com>
2019-04-16 11:06:24 +0300

    Created db_build.js relates #4

---
+++ database/db_build.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const dbConnection = require('./db_connection.js');
+let sqlPath = path.join(__dirname, 'db_build.sql');
+if(process.env.NODE_ENV === "test"){
+ sqlPath = path.join(__dirname,'..','tests','test_Db_Build.sql');
+}
+
+const sql = fs.readFileSync(sqlPath).toString();
+// dbConnection.query(sql, (err, res) => {
+//   if (err) throw err;
+//   console.log('Users table created with result: ', res);
+// });
+const runDbBuild = cb => dbConnection.query(sql,cb);
+runDbBuild((err,res)=>{
+    if (err) throw err;
+    console.log('successfully built the database');
+})
+module.exports = runDbBuild;
