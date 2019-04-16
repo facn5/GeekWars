9eed55bf4286f99fec2e106a9eb92e055fd40a5c
Tamer Nasser <tamernasser@outlook.com>
2019-04-16 13:05:28 +0300

    Added add user func SQL related #11

--- database/queries/queries.js
+++ database/queries/queries.js
@@ -3,3 +3,5 @@ const query = require('./sqlquery.js')
 const userExist = (username, cb) => query.select(`SELECT count(id) from users where username = '${username}'`, cb)
 
 const checkPassword = (username, password, cb) => query.select(`SELECT count(id) from users where username = '${username}' AND password = '${password}'`, cb)
+
+const addUser = (username,password,email,cb) => query.insert(`INSERT INTO users (username,password,email) VALUES ($1,$2,$3)`,[username,password,email],cb)
