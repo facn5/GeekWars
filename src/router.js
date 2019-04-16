cc42c2d81ac6c6e13a53d19394cd8fa120a4048c
mrfong <therealshadix@gmail.com>
2019-04-16 13:03:16 +0300

    added a sign in page, relates to #19

--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
-const url = require('url');	
-const handler = require('./handlers');	
+const url = require('url');
+const handler = require('./handlers');
 
- const router = (req, res) => {	
+ const router = (req, res) => {
 
-   let url = req.url;	
-  if (url === "/") {	
-    handler.home(res);	
-  } else if (url.indexOf('public') !== -1) {	
-  handler.public(url, res);	
-}	
-else{	
-  handler.error404(res);	
-}	
+   let url = req.url;
+  if (url === "/") {
+    handler.home(res);
+  } else if (url.includes('.')) {	
+  handler.public(url, res);
+}
+else{
+  handler.error404(res);
+}
 
- }	
+ }
 
- module.exports = router;	
+ module.exports = router;
