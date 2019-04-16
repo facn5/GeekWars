cec3e60bc83c3937f4064f31d8f1416a44acb6d1
mrfong <therealshadix@gmail.com>
2019-04-16 10:29:30 +0300

    set up the handlers.js file, exported the two handlers, relates to #1

--- src/handlers.js
+++ src/handlers.js
@@ -8,3 +8,36 @@ let extType = {
   js: { "content-type": "application/javascript" },
   json: { 'content-type': 'application/json' }
 }
+
+const handleHome = (res) => {
+  let pathFile = path.join(__dirname, "..", "public", "index.html");
+  fs.readFile(pathFile, (err, file) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("error500");
+    } else {
+      res.writeHead(200, extType.html);
+      res.end(file);
+    }
+  })
+}
+
+const handleError = (res) => {
+  let pathFile = path.join(__dirname, "..", "public", "404.html");
+  fs.readFile(pathFile, (err, file) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("error500");
+    } else {
+      res.writeHead(404, extType.html);
+      res.end(file);
+    }
+  })
+}
+
+
+module.exports = {
+  home: handleHome,
+  error: handleError
+
+}
