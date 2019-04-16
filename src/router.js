9a8716d221d3d1afd1e8e4333ad839bf7b078172
mrfong <therealshadix@gmail.com>
2019-04-16 10:36:27 +0300

    added the public assests handler to the router, relates to #1

--- src/router.js
+++ src/router.js
@@ -6,7 +6,9 @@ const router = (req, res) => {
   let url = req.url;
   if (url === "/") {
     handler.home(res);
-  }
+  } else if (url.indexOf('public') !== -1) {
+  handler.public(url, res);
+}
 else{
   handler.error404(res);
 }
