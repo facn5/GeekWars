34120ee46c85ac8b7a23c59bfdb9adcb85676355
Tamer Nasser <tamernasser@outlook.com>
2019-04-16 15:07:37 +0300

    Added case for signup related #26

--- src/router.js
+++ src/router.js
@@ -1,18 +1,21 @@
 const url = require('url');
 const handler = require('./handlers');
 
- const router = (req, res) => {
+const router = (req, res) => {
 
-   let url = req.url;
+  let url = req.url;
   if (url === "/") {
     handler.home(res);
-  } else if (url.includes('.')) {	
-  handler.public(url, res);
-}
-else{
-  handler.error404(res);
+  } else if (url.includes('.')) {
+    handler.public(url, res);
+  } else if(url === '/sign_up'){
+    if(req.method === 'POST'){
+      handler.signup(req,res);
+    }
+  } else {
+    handler.error404(res);
+  }
+
 }
 
- }
-
- module.exports = router;
+module.exports = router;
