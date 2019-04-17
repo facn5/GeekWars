f3dbc60f9d742972d4d79c23a3e3a083fe5ff019
Tamer Nasser <tamernasser@outlook.com>
2019-04-17 22:49:59 +0300

    If user is not authorized page will not be loaded related #43

--- public/scripts/getdata.js
+++ public/scripts/getdata.js
@@ -1,118 +1,129 @@
 
 verifyUser(function(data){
-  console.log(data)
+  if(data.status === 200){
+      var ans = document.querySelectorAll(".answer");
+      var skipQues = document.querySelector("#skip");
+      var waiting = document.querySelector("#waiting");
+      var loadAnim = document.querySelector("#waitanim");
+      var curScore = document.querySelector("#hscore").textContent;
+
+      function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
+       var question = getRandomInt(9);
+       // console.log(curScore);
+      var data;
+
+      // getRandomInt(9)
+      getData((dataq) => {
+        data = dataq;
+      	function updateQues(response) {
+
+        question++;
+
+
+
+      		document.querySelector("#question").innerHTML =dataq[question].question;
+      		document.querySelector("#optn1").innerHTML = dataq[question].option1;
+      		document.querySelector("#optn2").innerHTML = dataq[question].answer;
+      		document.querySelector("#optn3").innerHTML = dataq[question].option2;
+      		document.querySelector("#optn4").innerHTML = dataq[question].option3;
+      		// if(curScore>5){
+      		// 	console.log("you finished the game")
+      		// }
+
+      	}
+      		document.querySelector("#question").innerHTML =dataq[question].question;
+      		document.querySelector("#optn1").innerHTML = dataq[question].option1;
+      		document.querySelector("#optn2").innerHTML = dataq[question].answer;
+      		document.querySelector("#optn3").innerHTML = dataq[question].option2;
+      		document.querySelector("#optn4").innerHTML = dataq[question].option3;
+      		for (var i = 0; i < ans.length; i++) {
+      			ans[i].addEventListener("click", function() {
+      				if(question<9){
+
+      					checkAnswer(this);
+      				}
+      				else{
+      					alert("you finished all the questions, go to the leaderboard page to find your score!");
+      					curScore=0;
+
+      				}
+      			});
+      		}
+
+      		function checkAnswer(ele) {
+      			if (ele.innerHTML === dataq[question].answer) {
+      		//		ele.classList.add("correct");
+      				document.querySelector("#hscore").innerHTML = ++curScore;
+
+      			 updateQues();
+      			} else {
+      //				ele.classList.add("incorrect");
+      				// loading(true);
+      				document.querySelector("#hscore").innerHTML = --curScore;
+
+      			}
+      		}
+      		// console.log(dataq[0].question)
+      })
+
+
+
+      // for (var i = 0; i < ans.length; i++) {
+      // 	ans[i].addEventListener("click",function(){
+      // 		checkAnswer(this);
+      // 	});
+      // }
+      // function checkAnswer(ele) {
+      // 	if(ele.innerHTML === data.correct_answer){
+      // 		ele.classList.add("correct");
+      // 		loading(true);
+      // 		document.querySelector("#hscore").innerHTML = ++curScore;
+      // 		changeQues();
+      // 	}else {
+      // 		ele.classList.add("incorrect");
+      // 		loading(true);
+      // 		document.querySelector("#hscore").innerHTML = --curScore;
+      // 		changeQues();
+      // 	}
+      // }
+      // function changeQues() {
+      //
+      // 	setTimeout(function(){
+      // 		for (var j = 0; j < ans.length; j++) {
+      // 			ans[j].classList.remove("incorrect")
+      // 			ans[j].classList.remove("correct")
+      // 		}
+      // 		fetchReq();
+      // 	},800)
+      // }
+      // function loading(fire){
+      // 	if(fire){
+      // 		waiting.classList.add("loading");
+      // 		loadAnim.classList.add("animation");
+      // 	}else{
+      // 		waiting.classList.remove("loading");
+      // 		loadAnim.classList.remove("animation");
+      // 	}
+      // }
+      //
+      // skipQues.addEventListener("click",function() {
+      // 	loading(true);
+      // 	fetchReq();
+      // })
+      // fetchReq();
+
+  }else{
+    if (window.confirm('You are not logged in, do you want to make an account?'))
+  {
+      window.location.href = "/register.html"
+  }
+  else
+  {
+      window.location.href = "/login.html"
+  }
+  }
+
 });
-
-var ans = document.querySelectorAll(".answer");
-var skipQues = document.querySelector("#skip");
-var waiting = document.querySelector("#waiting");
-var loadAnim = document.querySelector("#waitanim");
-var curScore = document.querySelector("#hscore").textContent;
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
- var question = getRandomInt(9);
- console.log(curScore);
-var data;
-
-// getRandomInt(9)
-getData((dataq) => {
-  data = dataq;
-	function updateQues(response) {
-
-  question++;
-
-
-
-		document.querySelector("#question").innerHTML =dataq[question].question;
-		document.querySelector("#optn1").innerHTML = dataq[question].option1;
-		document.querySelector("#optn2").innerHTML = dataq[question].answer;
-		document.querySelector("#optn3").innerHTML = dataq[question].option2;
-		document.querySelector("#optn4").innerHTML = dataq[question].option3;
-		// if(curScore>5){
-		// 	console.log("you finished the game")
-		// }
-
-	}
-		document.querySelector("#question").innerHTML =dataq[question].question;
-		document.querySelector("#optn1").innerHTML = dataq[question].option1;
-		document.querySelector("#optn2").innerHTML = dataq[question].answer;
-		document.querySelector("#optn3").innerHTML = dataq[question].option2;
-		document.querySelector("#optn4").innerHTML = dataq[question].option3;
-		for (var i = 0; i < ans.length; i++) {
-			ans[i].addEventListener("click", function() {
-				if(question<9){
-
-					checkAnswer(this);
-				}
-				else{
-					alert("you finished all the questions, go to the leaderboard page to find your score!");
-					curScore=0;
-
-				}
-			});
-		}
-
-		function checkAnswer(ele) {
-			if (ele.innerHTML === dataq[question].answer) {
-		//		ele.classList.add("correct");
-				document.querySelector("#hscore").innerHTML = ++curScore;
-
-			 updateQues();
-			} else {
-//				ele.classList.add("incorrect");
-				// loading(true);
-				document.querySelector("#hscore").innerHTML = --curScore;
-
-			}
-		}
-		console.log(dataq[0].question)
-})
-
-
-
-// for (var i = 0; i < ans.length; i++) {
-// 	ans[i].addEventListener("click",function(){
-// 		checkAnswer(this);
-// 	});
-// }
-// function checkAnswer(ele) {
-// 	if(ele.innerHTML === data.correct_answer){
-// 		ele.classList.add("correct");
-// 		loading(true);
-// 		document.querySelector("#hscore").innerHTML = ++curScore;
-// 		changeQues();
-// 	}else {
-// 		ele.classList.add("incorrect");
-// 		loading(true);
-// 		document.querySelector("#hscore").innerHTML = --curScore;
-// 		changeQues();
-// 	}
-// }
-// function changeQues() {
-//
-// 	setTimeout(function(){
-// 		for (var j = 0; j < ans.length; j++) {
-// 			ans[j].classList.remove("incorrect")
-// 			ans[j].classList.remove("correct")
-// 		}
-// 		fetchReq();
-// 	},800)
-// }
-// function loading(fire){
-// 	if(fire){
-// 		waiting.classList.add("loading");
-// 		loadAnim.classList.add("animation");
-// 	}else{
-// 		waiting.classList.remove("loading");
-// 		loadAnim.classList.remove("animation");
-// 	}
-// }
-//
-// skipQues.addEventListener("click",function() {
-// 	loading(true);
-// 	fetchReq();
-// })
-// fetchReq();
