4e58ddf93fa939d3060ce4d3b3b42a9cb5e6a66b
Tamer Nasser <tamernasser@outlook.com>
2019-04-17 20:23:05 +0300

    added default question to work on later related #43

--- public/scripts/getdata.js
+++ public/scripts/getdata.js
@@ -2,26 +2,51 @@ var ans = document.querySelectorAll(".answer");
 var skipQues = document.querySelector("#skip");
 var waiting = document.querySelector("#waiting");
 var loadAnim = document.querySelector("#waitanim");
-var curScore = 0;
+var curScore = document.querySelector("#hscore").textContent;
+console.log(curScore);
 var data;
 
 
-getData((dataq)=>{
-data = dataq;
-console.log("front end",dataq);
+getData((dataq) => {
+  data = dataq;
+  document.querySelector("#question").innerHTML = dataq[0].question;
+  document.querySelector("#optn1").innerHTML = dataq[0].option1;
+  document.querySelector("#optn2").innerHTML = dataq[0].answer;
+  document.querySelector("#optn3").innerHTML = dataq[0].option2;
+  document.querySelector("#optn4").innerHTML = dataq[0].option3;
+  for (var i = 0; i < ans.length; i++) {
+    ans[i].addEventListener("click", function() {
+      checkAnswer(this);
+    });
+  }
+
+  function checkAnswer(ele) {
+    if (ele.innerHTML === dataq[0].answer) {
+      ele.classList.add("correct");
+      // loading(true);
+      document.querySelector("#hscore").innerHTML = ++curScore;
+      changeQues();
+    } else {
+      ele.classList.add("incorrect");
+      // loading(true);
+      document.querySelector("#hscore").innerHTML = --curScore;
+      changeQues();
+    }
+  }
+  console.log(dataq[0].question)
 
 })
 
 function updateQues(response) {
-	// data = response.data.results[0];
-	// document.querySelector("#question").innerHTML = data.question;
-	// var options = data.incorrect_answers;
-	// options.splice(randIndex(),0,data.correct_answer);
-	// document.querySelector("#optn1").innerHTML = ;
-	// document.querySelector("#optn2").innerHTML = ;
-	// document.querySelector("#optn3").innerHTML = ;
-	// document.querySelector("#optn4").innerHTML = ;
-	// loading(false);
+  // data = response.data.results[0];
+  // document.querySelector("#question").innerHTML = data.question;
+  // var options = data.incorrect_answers;
+  // options.splice(randIndex(),0,data.correct_answer);
+  // document.querySelector("#optn1").innerHTML = ;
+  // document.querySelector("#optn2").innerHTML = ;
+  // document.querySelector("#optn3").innerHTML = ;
+  // document.querySelector("#optn4").innerHTML = ;
+  // loading(false);
 }
 // for (var i = 0; i < ans.length; i++) {
 // 	ans[i].addEventListener("click",function(){
