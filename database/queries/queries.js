12be3c6e2b3068466b639c775bf63839ae199264
mrfong <therealshadix@gmail.com>
2019-04-17 23:48:17 +0300

    created a getscore fetch file for front end relates to #54

--- database/queries/queries.js
+++ database/queries/queries.js
@@ -11,6 +11,7 @@ const getQuestions = (cb) => query.select(`SELECT * from qa`,cb)
 const getPass = (username,cb) => query.select(`SELECT password from users where username = '${username}'`,cb);
 // const getScore = (username,cb) => query.select(`SELECT score from users where username='${username}'`)
 const getScore = (username,cb) => query.select(`SELECT score from users where username = '${username}'`,cb);
+
 const addScore = (username,score,cb) => query.update(`UPDATE users SET score = (SELECT score from users where username='$1')+$2 where username='$1'`,[username,score],cb)
 
 module.exports = {
