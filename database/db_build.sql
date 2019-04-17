3a552b0dae29f16da63691ef9c93ddd80d329536
mrfong <therealshadix@gmail.com>
2019-04-17 15:21:11 +0300

    added more questions to the datbase relates to#39

--- database/db_build.sql
+++ database/db_build.sql
@@ -24,6 +24,13 @@ INSERT INTO qa (question,answer,option1,option2,option3) VALUES
 ('What is the purpose of CASCADE in SQL','To drop all the related fields/table to the current table','To get the top row','To clear the database','To clear the current row'),
 ('What is Javascript','Programing language','Scripting language','markdown','not this'),
 ('What is the first?','what','me','you','we');
+('Which of the following isnt a Javascript datatype','String','Boolean','Object','document.write')
+('What is HTML','a pssst language','a markup language','an encoding script',' a scripting language');
+('What year was ES6 introudced','2015','2016','2017',' 2018');
+('How can you print in JS','console.log()','console.writeline()','system.println()',' print()');
+('What can you check in a string with in SQL','LIKE','INCLUDES','CHECK',' EXSISTS');
+('What can you choose all from a table in SQL','*','^','+',' /*/');
+('How can you import a module with NodeJS','require()','bring()','link()',' import()');
 
 CREATE TABLE IF NOT EXISTS leaderboard (
   id SERIAL PRIMARY KEY,
